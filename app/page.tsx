a54640ca32acad81e647bcb134f0b77be8c9cea3
dillonpw <dillonpwalsh10@gmail.com>
2024-11-07 20:49:58 -0500

    started planning in README

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { SignOut } from '@/components/signOut';
 export default function Home() {
     return (
         <main>
-            <p>hello</p>
             <SignIn />
             <SignOut />
         </main>
