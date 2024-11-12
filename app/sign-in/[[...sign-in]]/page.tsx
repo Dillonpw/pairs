4edde13b4ddced06cac44e5cd0768861f8ae3063
dillonpw <dillonpwalsh10@gmail.com>
2024-11-12 18:33:21 -0500

    testing auth, currently work

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from '@clerk/nextjs';
 
 export default function SignInPage() {
-  return <SignIn />;
+    return (
+        <main className='flex justify-center mt-20'>
+            <SignIn />
+        </main>
+    );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from '@clerk/nextjs';
 
 export default function SignInPage() {
-  return <SignUp />;
+    return (
+        <main className="flex justify-center mt-20">
+            <SignUp />
+        </main>
+    );
 }
