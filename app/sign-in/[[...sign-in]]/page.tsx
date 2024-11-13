ed9eecafd27dd7c7f0e55315c724d862c1f8c4f8
dillonpw <dillonpwalsh10@gmail.com>
2024-11-12 20:31:10 -0500

    removing clerk

--- app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/nextjs';
-
-export default function SignInPage() {
-    return (
-        <main className='flex justify-center mt-20'>
-            <SignIn />
-        </main>
-    );
-}
--- app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from '@clerk/nextjs';
-
-export default function SignInPage() {
-    return (
-        <main className="flex justify-center mt-20">
-            <SignUp />
-        </main>
-    );
-}
