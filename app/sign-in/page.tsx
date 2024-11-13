9572114e78c819312eff98aca2701c43628c575c
dillonpw <dillonpwalsh10@gmail.com>
2024-11-13 16:51:51 -0500

    dev auth environment working

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import SignIn from "@/components/sign-in";
+
+export default function SignInPage () {
+    return (
+        <main>
+            <p>hello</p>
+            <SignIn />
+        </main>
+    );
+}
