e35387a72709575d9ecb6d29522a59aba67bba07
dillonpw <dillonpwalsh10@gmail.com>
2024-11-16 13:04:26 -0500

    added sign in sign out

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,10 @@
+import SignIn from "@/components/sign-in";
+
 export default function AccountPage() {
     return (
         <main>
             <p>hello account page with controls for your profile</p>
+            <SignIn />
         </main>
     );
 }
